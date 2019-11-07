b06726d0b9f301b8928cd8eebd8fc645d144d71b
Bendik Mathias Johansen <bendikj@gmail.com>
2019-11-07 19:17:11 +0100

    Cleanse project with fruit plucked by virgins in the garden of life

---
+++ Src/Models/Database/Forum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Models.Database
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Owner { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
